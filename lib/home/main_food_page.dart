cb8f340de4bd91aba896c61f2acb1bfd82b99244
Amira Ameir <amiraameir@Amiras-MacBook-Pro.local>
2022-08-10 06:11:09 +0800

    page view builder

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,66 @@
+import 'package:ecomm_food/home/food_page_body.dart';
+import 'package:ecomm_food/widgets/big_text.dart';
+import 'package:ecomm_food/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          //showing the header
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: "Malaysia",
+                        color: AppColors.mainColor,
+                      ),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Penang",
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          //showing the body
+          FoodPageBody(),
+        ],
+      ),
+    );
+  }
+}
